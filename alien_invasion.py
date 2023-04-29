15934c3a5714e704f3f0421739da38b62ca01bba
TenetOhec <511605181@qq.com>
2023-04-29 18:06:17 +0800

    ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 	"""管理游戏资源和行为的类"""
@@ -16,6 +17,8 @@ class AlienInvasion:
 			(self.settings.screen_width,self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
 
+		self.ship = Ship(self)
+
 	
 	def run_game(self):
 		"""开始游戏的主循环"""
@@ -26,7 +29,8 @@ class AlienInvasion:
 					sys.exit()
 
 			#每次循环都重绘屏幕
-			self.screen.fill(self.settings.bg_color)		
+			self.screen.fill(self.settings.bg_color)
+			self.ship.blitme()		
 
 			#让最近绘制的屏幕可见
 			pygame.display.flip()
@@ -34,4 +38,4 @@ class AlienInvasion:
 if __name__ == '__main__':
 	#创建游戏实例并运行游戏
 	ai = AlienInvasion()
-	ai.run_game()						
+	ai.run_game()				
