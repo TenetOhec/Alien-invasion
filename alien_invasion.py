74f0071c679ed69a3aa1390df1250a577b4baae9
Zwm <511605181@qq.com>
2023-05-01 11:21:33 +0800

    finish bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,7 @@ class AlienInvasion:
 			#监视键盘和鼠标事件
 			self._check_events()
 			self.ship.update()
-			self.bullets.update()
+			self._update_bullets()
 			self._update_screen()		
 
 		
@@ -67,8 +67,19 @@ class AlienInvasion:
 
 	def _fire_bullet(self):
 		"""创建一颗子弹，并将其加入编组bullets中"""
-		new_bullet = Bullet(self)
-		self.bullets.add(new_bullet)		
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)	
+
+	def _update_bullets(self):
+		"""更新子弹的位置并删除消失的子弹"""
+		#更新子弹的位置
+		self.bullets.update()
+
+		#删除消失的子弹
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)				
 
 	def _update_screen(self):
 		"""更新屏幕上的图像，并切换到新屏幕"""
