c86bc78bd88ff2dcdf94d85995fb8aa526587dd2
Zwm <511605181@qq.com>
2023-05-02 14:09:03 +0800

    create aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
 	"""管理游戏资源和行为的类"""
@@ -25,6 +26,9 @@ class AlienInvasion:
 
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
+		self.aliens = pygame.sprite.Group()
+
+		self._create_fleet()
 
 	
 	def run_game(self):
@@ -79,14 +83,44 @@ class AlienInvasion:
 		#删除消失的子弹
 		for bullet in self.bullets.copy():
 			if bullet.rect.bottom <= 0:
-				self.bullets.remove(bullet)				
+				self.bullets.remove(bullet)	
 
+	def _create_fleet(self):
+		"""常见外星人群"""
+		#创建一个外星人并计算一行可容纳多少个外星人
+		#外星人的间距为外星人宽度
+		alien = Alien(self)
+		alien_width, alien_height = alien.rect.size
+
+		available_space_x = self.settings.screen_width - (2 * alien_width)
+		numbers_aliens_x = available_space_x // (2*alien_width)
+
+		#计算屏幕可容纳多少行外星人
+		ship_height = self.ship.rect.height
+		available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+		number_rows = available_space_y // (2 * alien_height)
+
+		#创建外星人群
+		for row_number in range(number_rows):
+			for alien_number in range(numbers_aliens_x):
+				self._create_alien(alien_number,row_number)
+
+	def _create_alien(self,alien_number,row_number):
+		"""创建一个外星人并将其放在当前行"""
+		alien = Alien(self)
+		alien_width, alien_height = alien.rect.size
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.x = alien.x
+		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+		self.aliens.add(alien)
+		
 	def _update_screen(self):
 		"""更新屏幕上的图像，并切换到新屏幕"""
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
+		self.aliens.draw(self.screen)	
 
 		pygame.display.flip()				
     
